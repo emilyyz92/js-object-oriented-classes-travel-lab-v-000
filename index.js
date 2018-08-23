c975089d328d15fcce11113ddca18e78e10fa440
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-23 17:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const convertMonth = function (string) {
   const words = string.split(' ')
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-
+  return months.filter(function (word) {
+    word === string.slice(0, word.length + 1)
+  }
+  )
 }
 
 class Driver {
