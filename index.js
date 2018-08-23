e0554203eead5e5c8a8353eb0d2de354c19e9f34
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-23 16:10:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ class Driver {
     this.name = name
     this.startDate = Date.parse(startDate)
   }
+
+  yearsExperienceFromBeginningOf (endDate) {
+    return endDate.getFullYear() - this.startDate.getFullYear() - 1;
+  }
 }
