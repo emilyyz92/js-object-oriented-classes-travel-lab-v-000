4a2c6432fced8b2c919ad05905fa1472e1be4d17
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-23 17:51:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const convertMonth = function (string) {
   const array = string.split(' ')
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
   return months.filter(function (word) {
-    words[0] === string.slice(0, word.length)
+    array[0] === string.slice(0, word.length)
   }
   )
 }
